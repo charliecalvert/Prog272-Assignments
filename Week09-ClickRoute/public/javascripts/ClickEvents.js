546bdabd94526cd366766aab73b12af892f775c4
Charlie Ubuntu Calvert <charlie@elvenware.com>
2015-03-09 16:05:00 -0700

    Assignments

---
+++ Week09-ClickRoute/public/javascripts/ClickEvents.js
@@ -0,0 +1,28 @@
+define(["jquery"], function(jquery) {'use strict';
+
+    var elf = {};
+    elf.run = {};
+
+    elf.ClickEvents = ( function() {
+            var listItem = $(".listItem");
+            var intro = $("#intro");
+
+            function ClickEvents() {
+                $(intro).html("ClickEvents is loaded. Click the three items seen below.");
+                $(intro).addClass('blue');
+                $(listItem).click(listClick);                
+            }
+
+            var listClick = function(event) {
+                var clickText = event.target.innerText;
+                var prompt = "You clicked: ";
+                $(intro).html(prompt + clickText);
+            };
+
+            return ClickEvents;
+
+        }());
+
+    return elf.ClickEvents;
+
+});
