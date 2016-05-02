e57dff58c48b81922a9e7dd8277e7a71538b012a
Charlie on RohanElf <charlie@elvenware.com>
2016-05-02 11:41:07 -0700

    Function this tests updated

--- FunctionThis/spec/test-basic.js
+++ FunctionThis/spec/test-basic.js
@@ -15,9 +15,39 @@ describe('Elvenware Simple Plain Suite', function () {
         expect(result).toBe(9);
     });
 
-    it('expects a function called get this to return the window object', function() {
+    it('expects a simple function called getThis to set this is the window object', function() {
         var result = getThis();
-        console.log(JSON.stringify(this));
         expect(result).toBe(window);
-    })
+    });
+
+    it('expects a simple anonymous function to set this to the window object', function() {
+        var result = getThisAnonymous();
+        expect(result).toBe(window);
+    });
+
+    it('expects a method of myObject called getThis to show this is myObject', function() {
+        var result = myObject.getThis();
+        expect(result).toBe(myObject);
+    });
+
+    it('expects a method of myFunction called getThis to show this is myFunction', function() {
+        var result = myFunction.getThis();
+        expect(result).toBe(myFunction);
+    });
+
+    it('expects a constructor function called MyFunction to have a public method called getThis that shows this is MyFunction', function() {
+        var myFunction = new MyFunction();
+        var result = myFunction.getThis();
+        expect(result).toBe(myFunction);
+    });
+
+    it('shows you can set the this operator for getThis to myObject', function() {
+        var result = getThis.call(myObject);
+        expect(result).toBe(myObject);
+    });
+
+    it('shows that return this from a simple strict function returns undefined', function() {
+        var result = getThisStrict();
+        expect(typeof result).toBe('undefined');
+    });
 });
