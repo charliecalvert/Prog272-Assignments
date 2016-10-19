249293d56cc589ebb1c7b53621edfb2c9397936f
Charlie on RohanElf <charlie@elvenware.com>
2016-10-18 20:55:27 -0700

    rename ClickRoute

---
+++ ClickRoute/routes/index.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(request, response) {
+    response.render('index', {
+        title: 'Click Routes'
+    });
+});
+
+router.get('/Item01', function(request, response) {
+    var result = {
+        "result": "Success"
+    };
+    response.send(result);
+});
+
+module.exports = router;
